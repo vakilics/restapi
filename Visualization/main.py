d32c8b38a6f9fdd6300f8f14f45e836b29b6f80d
Vakili, Abdul Rahman <vakili@univention.de>
2024-06-10 14:47:44 +0200

    feat: updated api

---
+++ Visualization/main.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+
+#Flask can create website objects
+
+app = Flask("Website")
+
+# when a user browses like:  <website-url>/home, myhome function will return then html page
+# so, put html file into a dir named: "templates"
+# http://127.0.0.1:5000/home
+
+@app.route("/home")
+def muhome():
+    return render_template("weather-api.html")
+
+
+@app.route("/about/")
+def about():
+    return render_template("about.html")
+
+app.run(debug=True)
+
